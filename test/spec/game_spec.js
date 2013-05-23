5e85ccfb11d200ef4a3e4a307ea04c2ca5a01c85
Pedro Cunha <pkunha@gmail.com>
2013-05-23 22:57:14 +0100

    Change string literals

--- test/spec/game_spec.js
+++ test/spec/game_spec.js
@@ -1,28 +1,28 @@
-describe("Game", function() {
+describe('Game', function() {
   var game;
 
-  describe("#start", function(){
+  describe('#start', function(){
     beforeEach(function(){
       game = new Game();
       game.start()
     })
 
-    it("prepares the current Tetromino", function(){
+    it('prepares the current Tetromino', function(){
       expect(game.currentTetromino).toBeDefined()
     })
 
-    it("prepares the next Tetromino", function(){
+    it('prepares the next Tetromino', function(){
       expect(game.nextTetromino).toBeDefined()
     })
 
-    it("allows to specify the current tetromino", function(){
+    it('allows to specify the current tetromino', function(){
       var tetromino = Tetromino.random();
 
       game.start({ currentTetromino: tetromino })
       expect(game.currentTetromino.coordinates).toEqual(tetromino.coordinates)
     })
 
-    it("allows to specify the next tetromino", function(){
+    it('allows to specify the next tetromino', function(){
       var tetromino = Tetromino.random();
 
       game.start({ nextTetromino: tetromino })
@@ -30,7 +30,7 @@ describe("Game", function() {
     })
   })
 
-  describe("#moveDown", function(){
+  describe('#moveDown', function(){
     var tetromino;
 
     beforeEach(function(){
@@ -39,11 +39,11 @@ describe("Game", function() {
       game.start({ currentTetromino: tetromino })
     })
 
-    it("returns true after start when can move to the first top row", function(){
+    it('returns true after start when can move to the first top row', function(){
       expect(game.moveDown()).toBe(true) 
     })
 
-    it("returns false when row 0th is all filled", function(){
+    it('returns false when row 0th is all filled', function(){
       // Fill the first row of grid with non-nil
       // values. moveDown returns false because
       // he can't move down. 
@@ -53,7 +53,7 @@ describe("Game", function() {
       expect(game.moveDown()).toBe(false);
     })
 
-    it("reflects moving down on the grid", function(){
+    it('reflects moving down on the grid', function(){
       game.moveDown()
 
       // - - - X X X - - - -
@@ -64,7 +64,7 @@ describe("Game", function() {
       expect(game.grid[0][6]).toBe(null);
     })
 
-    it("changes the grid state to reflect the twice movement", function(){
+    it('changes the grid state to reflect the twice movement', function(){
       game.moveDown()
       game.moveDown()
 
@@ -84,7 +84,7 @@ describe("Game", function() {
     })
   })
 
-  describe("#moveRight", function(){
+  describe('#moveRight', function(){
     var tetromino;
 
     beforeEach(function(){
@@ -93,11 +93,11 @@ describe("Game", function() {
       game.start({ currentTetromino: tetromino })
     })
 
-    it("returns true when can move to the right", function(){
+    it('returns true when can move to the right', function(){
       expect(game.moveRight()).toBe(true)
     })
 
-    it("returns false if can not move to the right", function(){
+    it('returns false if can not move to the right', function(){
       game.moveDown()
       game.moveRight(); // - - - - X X X - - -
       game.moveRight(); // - - - - - X X X - - 
@@ -106,7 +106,7 @@ describe("Game", function() {
       expect(game.moveRight()).toBe(false); 
     })
 
-    it("moves the whole tetromino", function(){
+    it('moves the whole tetromino', function(){
       game.moveDown()
       game.moveDown();
       game.moveRight();
@@ -126,7 +126,7 @@ describe("Game", function() {
       expect(game.grid[1][7]).toBe(null);
     })
 
-    it("moves to the right till gets to a complex tetromino", function(){
+    it('moves to the right till gets to a complex tetromino', function(){
       game.grid[0][6] = 1
       game.grid[0][7] = 1
       game.grid[1][7] = 1
@@ -143,7 +143,7 @@ describe("Game", function() {
       expect(game.moveRight()).toBe(false)
     })
 
-    it("moves to the right till gets to a complex tetromino (case 2)", function(){
+    it('moves to the right till gets to a complex tetromino (case 2)', function(){
       game.grid[0][6] = 1
       game.grid[0][7] = 1
       game.grid[0][8] = 1
@@ -166,7 +166,7 @@ describe("Game", function() {
       expect(game.moveRight()).toBe(false)
     })
 
-    it("returns false if can not move to the right due to a block", function(){
+    it('returns false if can not move to the right due to a block', function(){
       game.grid[0][6] = 1
       game.grid[1][6] = 1
 
@@ -178,11 +178,11 @@ describe("Game", function() {
       expect(game.moveRight()).toBe(false)
     })
 
-    it("returns true when its still vertically outside of the grid but still horizontally inside", function(){
+    it('returns true when its still vertically outside of the grid but still horizontally inside', function(){
       expect(game.moveRight()).toBe(true)
     })
 
-    it("returns false when its vertically outside but at the right edge of the grid", function(){
+    it('returns false when its vertically outside but at the right edge of the grid', function(){
       game.moveRight();
       game.moveRight();
       game.moveRight();
@@ -201,12 +201,12 @@ describe("Game", function() {
       game.start()
     })
 
-    it("returns true for any row when game starts", function(){
+    it('returns true for any row when game starts', function(){
       for(var i = 0; i < Game.VERTICAL_SPACES; ++i)
         expect(game.lineIsEmpty(i)).toBe(true)
     })
 
-    it("returns false if row contains at least one non-null object", function(){
+    it('returns false if row contains at least one non-null object', function(){
       game.grid[1][5] = {}
       expect(game.lineIsEmpty(1)).toBe(false)
     })
--- test/spec/tetromino_spec.js
+++ test/spec/tetromino_spec.js
@@ -1,40 +1,40 @@
-describe("Tetromino", function() {
+describe('Tetromino', function() {
   var tretomino;
 
-  describe("#new", function(){
+  describe('#new', function(){
     beforeEach(function() {
       // oxx/xxo
       tretomino = new Tetromino([[1,0], [2,0], [0,1], [1, 1]]);
     });
 
-    it("allows access to its composition", function(){
+    it('allows access to its composition', function(){
       expect(tretomino.coordinates).toBeDefined();
     });
 
-    it("sets a default color", function(){
+    it('sets a default color', function(){
       expect(tretomino.color).toEqual('black')
     })
 
-    it("initializes a grid with 2 rows and 3 columns", function(){
+    it('initializes a grid with 2 rows and 3 columns', function(){
       expect(tretomino.grid[0].length).toEqual(3)
       expect(tretomino.grid[1].length).toEqual(3)
       expect(tretomino.grid.length).toEqual(2)
     })
   })
 
-  describe(".all", function(){
-    it("has all 7 combinations", function(){
+  describe('.all', function(){
+    it('has all 7 combinations', function(){
       expect(Tetromino.all.length).toEqual(7)
     })
   });
 
-  describe(".random", function(){
-    it("returns a random Tetromino", function(){
+  describe('.random', function(){
+    it('returns a random Tetromino', function(){
       expect(Tetromino.random()).toEqual(jasmine.any(Object))
     })
   })
 
-  describe("#clone", function(){
+  describe('#clone', function(){
     var clone;
     
     beforeEach(function(){
@@ -42,51 +42,51 @@ describe("Tetromino", function() {
       clone     = tetromino.clone();
     })
 
-    it("copies coordinates", function(){
+    it('copies coordinates', function(){
       expect(clone.coordinates).toEqual([[0,0]])
     })
 
-    it("copies color", function(){
+    it('copies color', function(){
       expect(clone.color).toEqual('pink')
     })
 
-    it("doesnt return self", function(){
+    it('doesnt return self', function(){
       expect(clone).not.toBe(tetromino)
     })
   })
 
-  describe("#toSVG", function(){
+  describe('#toSVG', function(){
     var svg;
 
     beforeEach(function(){
       svg = Tetromino.all[0].toSVG();
     })
 
-    it("returns a svg with the correct namespace", function(){
-      expect($(svg).attr("xmlns")).toEqual("http://www.w3.org/2000/svg")
+    it('returns a svg with the correct namespace', function(){
+      expect($(svg).attr('xmlns')).toEqual("http://www.w3.org/2000/svg")
     })
 
-    it("returns a svg with 4 rect elements", function(){
+    it('returns a svg with 4 rect elements', function(){
       var els = _.map($(svg).children(), function(i){ return i.tagName })
       expect(els).toEqual(['rect','rect','rect','rect'])
     })
   })
 
 
-  describe("#width", function(){
-    it("returns 4 as width if it is 4 blocks wide", function(){
+  describe('#width', function(){
+    it('returns 4 as width if it is 4 blocks wide', function(){
       tetromino = new Tetromino([[0,0], [1,0], [2,0], [3, 0]]), // xxxx
       expect(tetromino.width()).toEqual(4)
     })
 
-    it("correctly calculates the width of 3 for a block with 2 levels", function(){
+    it('correctly calculates the width of 3 for a block with 2 levels', function(){
       tetromino = new Tetromino([[1,0], [2,0], [0,1], [1, 1]]), // oxx/xxo
       expect(tetromino.width()).toEqual(3)
     })
   })
 
-  describe ("#height", function(){
-    it("returns 2 as height if its 2 row wide", function(){
+  describe ('#height', function(){
+    it('returns 2 as height if its 2 row wide', function(){
       tetromino = new Tetromino([[0,0], [1,0], [1,0], [1, 1]]), // xx/xx
       expect(tetromino.height()).toEqual(2)
     })
